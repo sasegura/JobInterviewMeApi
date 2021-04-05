e1d9fd2eb0e577ca0ff4fac379dbae64406061f2
ighilarion <ighilarion.1990@gmail.com>
2021-04-05 16:53:04 +0200

    asd

--- src/datasources/postgres.datasource.ts
+++ src/datasources/postgres.datasource.ts
@@ -8,8 +8,8 @@ const config = {
   host: 'localhost',
   port: 5432,
   user: 'postgres',
-  password: 'root',
-  database: 'jobInterView'
+  password: 'postgres',
+  database: 'JobInterView'
 };
 
 // Observe application's life cycle to disconnect the datasource when
